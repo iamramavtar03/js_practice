3f25facca301959f0f0b7f6fafe0ba1ee6fb3c22
iamramavtar03 <70501808+iamramavtar03@users.noreply.github.com>
2025-02-27 21:03:03 +0530

    check
    .

--- 01_basic/test.js
+++
@@ -1,9 +0,0 @@
-
-console.log("Ram Avtar");
-console.log("Ram Avtar");
-console.log("Ram Avtar");
-console.log("Ram Avtar");
-console.log("Ram Avtar");
-console.log("Ram Avtar");
-console.log("Ram Avtar");
-console.log("Ram Avtar");
